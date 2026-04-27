cc664c8499d6fafa9fd158fbef65ae2921b1b07a
guidoyannitto <guido.yannitto@gmail.com>
2026-04-27 20:33:36 -0300

    remove local build files

--- .next/dev/static/chunks/app_page_tsx_0yv1csw._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/_0g1.vxy._.js","static/chunks/node_modules_0d57f74._.js"],
-    source: "dynamic"
-});
